f5f7ae11283f0e95db0c27e78971317ca4e96111
Eduard <spoljar.eduard@gmail.com>
2022-01-05 08:25:57 +0100

    Create send-contact.php

---
+++ send-contact.php
@@ -0,0 +1,70 @@
+<?php
+echo'
+	<main>
+		<h1>Contact Form</h1>
+		<div id="contact">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2780.324007373785!2d16.1327978154944!3d45.824795117330545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476679e469013b0f%3A0x678fa4715db6ea14!2sFu%C4%8Deki%20ul.%2026%2C%2010360%2C%20Sesvete!5e0!3m2!1sen!2shr!4v1607511411150!5m2!1sen!2shr" width="100%" height="510" frameborder="0" allowfullscreen="2" aria-hidden="false" tabindex="0"></iframe>
+			<?php';
+				print '<p style="text-align:center; padding: 10px; background-color: #d7d6d6;border-radius: 5px;">We recieved your question. We will answer within 24 hours.</p>';
+				$EmailHeaders  = "MIME-Version: 1.0\r\n";
+				$EmailHeaders .= "Content-type: text/html; charset=utf-8\r\n";
+				$EmailHeaders .= "From: <espolj2@vvg.hr>\r\n";
+				$EmailHeaders .= "Reply-To:<espolj2@vvg.hr>\r\n";
+				$EmailHeaders .= "X-Mailer: PHP/".phpversion();
+				$EmailSubject = 'Example page - Contact Form';
+				$EmailBody  = '
+				<html>
+				<head>
+				   <title>'.$EmailSubject.'</title>
+				   <style>
+					body {
+					  background-color: #ffffff;
+						font-family: Arial, Helvetica, sans-serif;
+						font-size: 16px;
+						padding: 0px;
+						margin: 0px auto;
+						width: 500px;
+						color: #000000;
+					}
+					p {
+						font-size: 14px;
+					}
+					a {
+						color: #00bad6;
+						text-decoration: underline;
+						font-size: 14px;
+					}
+					
+				   </style>
+				   </head>
+				<body>
+					<p>First name: ' . $_POST['firstname'] . '</p>
+					<p>Last name: ' . $_POST['lastname'] . '</p>
+					<p>E-mail: <a href="mailto:' . $_POST['email'] . '">' . $_POST['email'] . '</a></p>
+					<p>Country: ' . $_POST['country'] . '</p>
+					<p>Subject: ' . $_POST['subject'] . '</p>
+				</body>
+				</html>';
+				print '<p>First name: ' . $_POST['firstname'] . '</p>
+				<p>Last name: ' . $_POST['lastname'] . '</p>
+				<p>E-mail: ' . $_POST['email'] . '</p>
+				<p>Country: ' . $_POST['country'] . '</p>
+				<p>Subject: ' . $_POST['subject'] . '</p>';
+				mail($_POST['email'], $EmailSubject, $EmailBody, $EmailHeaders);
+			?>
+		</div>
+	</main>
+	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-98455037-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+	
+</body>
+</html>
