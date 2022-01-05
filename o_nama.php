8aeaad0621cd2ed4032b67562173e1264412db77
Eduard <spoljar.eduard@gmail.com>
2022-01-05 08:28:31 +0100

    Create o_nama.php

---
+++ o_nama.php
@@ -0,0 +1,14 @@
+<?php
+echo'
+    <div style="width: 75%; text-align: justify; margin-left: 12%;">
+    <h2>MAJSTOR.hr</h2>
+    <p>Na hrvatskom tržištu postojimo već dvadeset godina. Počeli smo kao elektroinstalaterski obrt. Tada smo u svojoj ponudi uz izradu elektroinstalacija jake i slabe struje imali i montažu rasvjetniha tijela.</p>
+    <p>Međutim, zbog situacije na tržištu širimo svoj posao na ostale završne građevinske radove. Danas smo jedna od vodečih tvrtki na području instalacija jake i slabe struje, vode i kanalizacije, grijanja i klimatizacije, suhe gradnje te manjih zidarskih radova.</p>
+    <p>Timovi naših arhitekata i projektanata svakom će objektu dati funkcionalan i visoko estetski izgled.</p>
+    <p>Cilj nam je daljnje usavršavanje u smjeru novih tehnologija i trendova u području završnih građevinskih radova. Nadalje, želimo ulagati u svoje djelatnike, u njihovo usavršavanje i edukaciju kako bismo mogli što bolje izvesti naše radove.</p>
+    <p>Garantiramo Vam visoku kvalitetu izrade!</p>
+    <p>Nikada nas ne zovite da vam lijepimo <strong>tapete!</strong></p>
+    <video width="480" height="360" controls>
+    <source src="img/ajeto.mp4" type="video/mp4"> 
+    </video>
+    </div>';
